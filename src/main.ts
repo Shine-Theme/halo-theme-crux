2935c4ea59859693e213e6180adf816c9bcfe1dd
Alkaid <GeorgeYuan233@gmail.com>
2023-07-11 01:47:52 +0800

    fix(nav): Fix navbar mobile adaptation(#13)
    - Added nav submenu
    - Allowed nav menu overflow
    - Added user icon
    
    Closed #13

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: Alkaid(AlkaidMegrez@outlook.com)
  * @Date: 2023-07-09 03:01:53
  * @LastEditors: Alkaid(AlkaidMegrez@outlook.com)
- * @LastEditTime: 2023-07-11 00:38:23
+ * @LastEditTime: 2023-07-11 01:47:10
  * @FilePath: /theme-crux/src/main.ts
  * @Description: 
  * 
@@ -13,27 +13,29 @@ import "./styles/main.css";
 import Alpine from "alpinejs";
 import * as tocbot from 'tocbot'
 import github from "./data/github";
+import dropdown from "./data/dropdown";
 
 window.Alpine = Alpine;
 
 Alpine.data("github", github);
+Alpine.data("dropdown", dropdown);
 
 Alpine.start();
 
 export function count(x: number, y: number) {
   return x + y;
 }
-export function generateToc(content_id: string, toc_container_class: string, toc_class: string) {
+export function generateToc(content_id: string,toc_container_class: string,toc_class: string) {
   const content = document.getElementById(content_id);
   const titles = content?.querySelectorAll("h1, h2, h3, h4");
   if (!titles || titles.length === 0) {
-    const tocContainer = document.querySelector("." + toc_container_class);
+    const tocContainer = document.querySelector("."+toc_container_class);
     tocContainer?.remove();
     return;
   }
   tocbot.init({
-    tocSelector: "." + toc_class,
-    contentSelector: "#" + content_id,
+    tocSelector: "."+toc_class,
+    contentSelector: "#"+content_id,
     headingSelector: "h1, h2, h3, h4",
     extraListClasses: "space-y-1 dark:border-slate-500",
     extraLinkClasses:
