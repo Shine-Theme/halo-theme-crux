a972f1c43f1258fb312dd95f8c1381aec06acbf8
Alkaid <GeorgeYuan233@gmail.com>
2023-07-11 01:46:56 +0800

    build(npm): Switch the import of Alpine.js and Axios from CDN to npm.
    - Removed CDN require
    - Added Alpinejs and axios dependencies
    - Refactor GitHub page code

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,39 @@
+/*
+ * @Author: Alkaid(AlkaidMegrez@outlook.com)
+ * @Date: 2023-07-09 03:01:53
+ * @LastEditors: Alkaid(AlkaidMegrez@outlook.com)
+ * @LastEditTime: 2023-07-11 00:38:23
+ * @FilePath: /theme-crux/src/main.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by Crux, All Rights Reserved. 
+ */
 import "./styles/tailwind.css";
 import "./styles/main.css";
 import Alpine from "alpinejs";
 import * as tocbot from 'tocbot'
+import github from "./data/github";
 
 window.Alpine = Alpine;
+
+Alpine.data("github", github);
 
 Alpine.start();
 
 export function count(x: number, y: number) {
   return x + y;
 }
-export function generateToc(content_id: string,toc_container_class: string,toc_class: string) {
+export function generateToc(content_id: string, toc_container_class: string, toc_class: string) {
   const content = document.getElementById(content_id);
   const titles = content?.querySelectorAll("h1, h2, h3, h4");
   if (!titles || titles.length === 0) {
-    const tocContainer = document.querySelector("."+toc_container_class);
+    const tocContainer = document.querySelector("." + toc_container_class);
     tocContainer?.remove();
     return;
   }
   tocbot.init({
-    tocSelector: "."+toc_class,
-    contentSelector: "#"+content_id,
+    tocSelector: "." + toc_class,
+    contentSelector: "#" + content_id,
     headingSelector: "h1, h2, h3, h4",
     extraListClasses: "space-y-1 dark:border-slate-500",
     extraLinkClasses:
