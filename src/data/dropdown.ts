3bb72e99780979445f082ed9eb35c98c3649fe22
Alkaid <GeorgeYuan233@gmail.com>
2023-07-11 19:11:24 +0800

    style: :lipstick: use prettier to optimize code style.
    closed #2

--- src/data/dropdown.ts
+++ src/data/dropdown.ts
@@ -4,19 +4,19 @@
  * @LastEditors: Alkaid(AlkaidMegrez@outlook.com)
  * @LastEditTime: 2023-07-11 00:38:14
  * @FilePath: /theme-crux/src/data/dropdown.ts
- * @Description: 
- * 
- * Copyright (c) 2023 by Crux, All Rights Reserved. 
+ * @Description:
+ *
+ * Copyright (c) 2023 by Crux, All Rights Reserved.
  */
 
 export default () => ({
-    show: false,
-    toggle() {
-        this.show = !this.show
-        console.log(this.show)
-    },
-    toggle2() {
-        this.show = !this.show
-        console.log(this.show)
-    }
-})
+  show: false,
+  toggle() {
+    this.show = !this.show;
+    console.log(this.show);
+  },
+  toggle2() {
+    this.show = !this.show;
+    console.log(this.show);
+  },
+});
--- src/data/github.ts
+++ src/data/github.ts
@@ -2,37 +2,46 @@
  * @Author: Alkaid(AlkaidMegrez@outlook.com)
  * @Date: 2023-07-11 00:08:17
  * @LastEditors: Alkaid(AlkaidMegrez@outlook.com)
- * @LastEditTime: 2023-07-11 00:36:50
+ * @LastEditTime: 2023-07-11 18:00:13
  * @FilePath: /theme-crux/src/data/github.ts
  * @Description: 当前解决方案会泄漏github_token，不建议使用，即将弃用
- * 
- * Copyright (c) 2023 by Crux, All Rights Reserved. 
+ *
+ * Copyright (c) 2023 by Crux, All Rights Reserved.
  */
 
-
-import axios from 'axios'
+import axios from "axios";
 export default () => ({
-    loading: true,
-    userinfo: {},
-    repos: {},
-    get(github_username: string, github_token: string) {
-        axios.get('https://api.github.com/users/' + github_username, {
+  loading: true,
+  error: false,
+  userinfo: {},
+  repos: {},
+  get(github_username: string, github_token: string) {
+    axios
+      .get("https://api.github.com/users/" + github_username, {
+        headers: {
+          Authorization: "token " + github_token,
+        },
+      })
+      .then((response) => {
+        this.userinfo = response.data;
+        axios
+          .get(response.data.repos_url, {
             headers: {
-                Authorization: 'token ' + github_token
-            }
-        }).then((response) => {
-            this.userinfo = response.data
-            axios.get(this.userinfo.repos_url, {
-                headers: {
-                    Authorization: 'token ' + github_token
-                }
-            }).then((response) => {
-                this.repos = response.data
-                this.loading = false
-
-            }).catch((error) => {
-            })
-        }).catch((error) => {
-        })
-    }
-})
+              Authorization: "token " + github_token,
+            },
+          })
+          .then((response) => {
+            this.repos = response.data;
+            this.loading = false;
+          })
+          .catch(() => {
+            this.loading = false;
+            this.error = true;
+          });
+      })
+      .catch(() => {
+        this.loading = false;
+        this.error = true;
+      });
+  },
+});
