039aac534e474cc355e4234a761169d80e522eb4
Alkaid <GeorgeYuan233@gmail.com>
2023-07-16 22:11:00 +0800

    feat(social): :sparkles: added a variety of new social media sharing options
    closed #36

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: Alkaid(AlkaidMegrez@outlook.com)
  * @Date: 2023-07-09 03:01:53
  * @LastEditors: Alkaid(AlkaidMegrez@outlook.com)
- * @LastEditTime: 2023-07-11 15:06:12
+ * @LastEditTime: 2023-07-16 21:33:14
  * @FilePath: /theme-crux/src/main.ts
  * @Description:
  *
@@ -15,6 +15,7 @@ import github from "./data/github";
 import dropdown from "./data/dropdown";
 import toc from "./data/toc";
 import darkmod from "./data/darkmod";
+import social from "./data/social";
 
 window.Alpine = Alpine;
 
@@ -22,4 +23,5 @@ Alpine.data("github", github);
 Alpine.data("dropdown", dropdown);
 Alpine.data("toc", toc);
 Alpine.data("darkmod", darkmod);
+Alpine.data("social", social);
 Alpine.start();
